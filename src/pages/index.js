7742fbbfa453d2b2b655ea9875a0e97d5136cc28
Michael Carr <xyeres@me.com>
2022-05-12 14:53:12 -0700

    Setup sass Fixes #5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import '../sass/styles.scss';
 
 // markup
 const IndexPage = () => {
   return (
     <main>
      <h1>Cynthia Cao!</h1>
+     <button className="btn-primary">Click Me!</button>
     </main>
   )
 }
