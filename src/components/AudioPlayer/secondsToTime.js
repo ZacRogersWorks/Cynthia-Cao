cc78921782df70dfaa0d266d2e2607a1b9251fe4
Michael Carr <xyeres@me.com>
2022-05-14 15:13:22 -0700

    Change default output

--- src/components/AudioPlayer/secondsToTime.js
+++ src/components/AudioPlayer/secondsToTime.js
@@ -1,5 +1,5 @@
 export default function secondsToTime(time) {
-  if (!time || isNaN(time)) return '--:--'
+  if (!time || isNaN(time)) return '0:00'
   let m = Math.floor(time % 3600 / 60).toString().padStart(1, '0'),
     s = Math.floor(time % 60).toString().padStart(2, '0');
   return `${m}:${s}`;
